97a845f1c1ada82e5bc0a63324371462001c7a4f
AdrianGabryla <adriangabryla@gmail.com>
2024-10-29 21:26:00 +0100

    Add project files.

---
+++ Simulator/Creature.cs
@@ -0,0 +1,24 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    private string name="Unknown";
+    private int level;
+    public string Name 
+    { 
+        get { return name; } 
+    }
+    public int Level
+    {
+        get { return level; }
+        set { level = value > 0 ? value : 1; }
+    }
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
+    }
+    public string Info
+    {
+        get { return $"{name} [{level}]"; }
+    }
+}
