26ca0df0e94552307a3a38947f6a41772595b648
AdrianGabryla <adriangabryla@gmail.com>
2024-10-31 12:11:12 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,10 +2,37 @@
 
 public class Animals
 {
-    public required string Description { get; init; }
-    public uint Size { get; set; } = 3;
+    private string description = "Unknown";
+    public required string Description
+    {
+        get { return description; }
+        init
+        {
+            var trimmed = value.Trim();
+            if (trimmed.Length < 3)
+            {
+                trimmed = trimmed.PadRight(3, '#');
+            }
+            else if (trimmed.Length > 15)
+            {
+                trimmed = trimmed.Substring(0, 15);
+            }
+            if (!char.IsUpper(trimmed[0]))
+            {
+                trimmed = char.ToUpper(trimmed[0]) + trimmed.Substring(1);
+            }
+            trimmed = trimmed.Trim();
+            if (trimmed.Length < 3)
+            {
+                trimmed = trimmed.PadRight(3, '#');
+            }
+            description = trimmed;
+        }
+
+    }
     public string Info
     {
         get { return $"{Description} <{Size}>"; }
     }
+    public uint Size { get; set; } = 3;
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,24 +1,77 @@
 ﻿namespace Simulator;
 
-public class Creature
+internal class Creature
 {
-    private string name="Unknown";
-    private int level;
-    public string Name 
-    { 
-        get { return name; } 
-    }
-    public int Level
-    {
-        get { return level; }
-        set { level = value > 0 ? value : 1; }
-    }
-    public void SayHi()
-    {
-        Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
-    }
-    public string Info
-    {
-        get { return $"{name} [{level}]"; }
-    }
+        private string name = "Unknown";
+        public string Name
+        {
+            get { return name; }
+            init
+            {
+                var trimmed = value.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                else if (trimmed.Length > 25)
+                {
+                    trimmed = trimmed.Substring(0, 25);
+                }
+                if (!char.IsUpper(trimmed[0]))
+                {
+                    trimmed = char.ToUpper(trimmed[0]) + trimmed.Substring(1);
+                }
+                trimmed = trimmed.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+
+
+                name = trimmed;
+
+            }
+        }
+
+        private int level = 1;
+        public int Level
+        {
+            get { return level; }
+            init
+            {
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                else if (value > 10)
+                {
+                    value = 10;
+                }
+                level = value;
+            }
+        }
+
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+
+        }
+        public Creature() { }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi I'm {Name} and my level is {Level}");
+        }
+        public void Upgrade()
+        {
+            if (Level < 10)
+            {
+                level++;
+            }
+        }
+        public string Info
+        {
+            get { return $"{Name} [{Level}]"; }
+        }
 }
+
